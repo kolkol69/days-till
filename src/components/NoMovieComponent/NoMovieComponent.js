df57f6e67c3a2b0fb3ceca3ff5345e48019eebf2
kolkol <kolodiy1103@gmail.com>
2019-12-06 17:16:21 +0100

    Create additional presentational component

---
+++ src/components/NoMovieComponent/NoMovieComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NoMovieComponent() {
+  return (
+    <div>
+      You did not add any movies to favorite
+      <span role="img" aria-label="sad face">😐</span>
+    </div>
+  );
+}
